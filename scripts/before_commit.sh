17b76183c8958ff2766b27c37a031ac411887907
Vlad Emelianov <vlad.emelianov.nz@gmail.com>
2025-03-02 23:38:28 +0300

    Fix sanity run

--- scripts/before_commit.sh
+++ scripts/before_commit.sh
@@ -7,4 +7,5 @@ cd $ROOT_PATH
 # uvx vulture src/istub --make-whitelist > vulture_whitelist.txt
 uvx pre-commit run --all-files
 
+uvx --python 3.8 --with . --with pip istub -buid
 # uv run pytest --cov-report html --cov src/istub
